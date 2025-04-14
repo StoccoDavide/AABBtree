20255c45baf1ff8c92fa5fa1ce8fe0bf24753344
Davide Stocco <davide.stocco@unitn.it>
2025-04-14 09:29:25 +0200

    Bug fix on github action test

--- cmake/test_cmake_inclusion/test.cc
+++ cmake/test_cmake_inclusion/test.cc
@@ -3,10 +3,10 @@
 int main()
 {
   AABBtree::Box<double, 2> box_d;
-  AABBtree::Box<float, 2>  box_f;
+  AABBtree::Box<float, 2> box_f;
   AABBtree::Ray<double, 2> ray_d;
-  AABBtree::Ray<float, 2>  ray_f;
-  AABBtree::AABBtree<double, 2> tree_d;
-  AABBtree::AABBtree<float, 2>  tree_f;
+  AABBtree::Ray<float, 2> ray_f;
+  AABBtree::Tree<double, 2> tree_d;
+  AABBtree::Tree<float, 2> tree_f;
   return 0;
 }
