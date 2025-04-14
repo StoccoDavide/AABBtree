2f503a80e1495f7ebab72d7799bf82122b142575
Davide Stocco <davide.stocco@unitn.it>
2025-04-14 09:23:28 +0200

    Add github actions

--- cmake/test_cmake_inclusion/test.cc
+++ cmake/test_cmake_inclusion/test.cc
@@ -1,8 +1,12 @@
 #include "AABBtree.hh"
-#include <iostream>
 
 int main()
 {
-  AABBtree::AABBtree<double> tree;
+  AABBtree::Box<double, 2> box_d;
+  AABBtree::Box<float, 2>  box_f;
+  AABBtree::Ray<double, 2> ray_d;
+  AABBtree::Ray<float, 2>  ray_f;
+  AABBtree::AABBtree<double, 2> tree_d;
+  AABBtree::AABBtree<float, 2>  tree_f;
   return 0;
 }
